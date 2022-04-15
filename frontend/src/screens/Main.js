5fc0199780696e8de0dc807971cb35be333ac198
arTzach <ar.tzachristas@gmail.com>
2022-04-15 13:20:33 +0300

    Product component

--- frontend/src/screens/Main.js
+++ frontend/src/screens/Main.js
@@ -1,33 +1,19 @@
-import { Link } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Product from "../componets/Product";
 function Main(products) {
   return (
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {products.map((product) => (
-          <div className="product" key={product.slug}>
-            <Link to={`/product/${product.slug}`}>
-              <img src={product.image} alt={product.name} />
-            </Link>
-            <div className="product-info">
-              <Link to={`/product/${product.slug}`}>
-                <p>{product.name}</p>
-              </Link>
-              <p>
-                <strong>${product.price}</strong>
-              </p>
-              <button type="button" className="btn btn-default btn-sm">
-                <span
-                  className="glyphicon 
-                    glyphicon-shopping-cart"
-                ></span>
-                <b> Add to Cart </b>
-              </button>
-            </div>
-          </div>
-        ))}
+        <Row>
+          {products.map((product) => (
+            <Col sm={6} md={4} ls={3} className="mb-3">
+              <Product product={product}></Product>
+            </Col>
+          ))}
+        </Row>
       </div>
     </div>
   );
